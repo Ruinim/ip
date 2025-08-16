03c44ced37ac678c92153c590e2fe69da78d12de
Ruinim <89561179+Ruinim@users.noreply.github.com>
2025-08-16 18:02:37 +0800

    Add add and list

--- src/main/java/Reim.java
+++ src/main/java/Reim.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Reim {
     public static void main(String[] args) {
@@ -12,13 +13,22 @@ public class Reim {
         System.out.println(logo);
 
         boolean stop = false;
+        ArrayList<String> items = new ArrayList<String>();
         while (!stop) {
             Scanner read = new Scanner(System.in);
             String command = read.nextLine();
-            String output = message(command);
-            if (command.equals("bye")) {
+            String output = message("added: " + command);
+            Integer commandType = commandParse(command);
+
+            if (commandType.equals(1)) {
                 output = end;
                 stop = true;
+            } else if (commandType.equals(2)) {
+                output = message(listOutput(items));
+            }
+            else {
+                // not any of the commands --> add to list
+                items.add(command);
             }
             System.out.println(output);
         }
@@ -32,5 +42,23 @@ public class Reim {
         return "____________________________________________________________\n"
                 + msg + "\n"
                 + "____________________________________________________________\n";
+    }
+
+    public static Integer commandParse(String command) {
+        if (command.equals("bye")) {
+            return 1;
+        }
+        else if (command.equals("list")) {
+            return 2;
+        }
+        return 0;
+    }
+
+    public static String listOutput(ArrayList<String> arr) {
+        StringBuilder finalString = new StringBuilder();
+        for (int i = 1; i - 1 < arr.size(); i++) {
+            finalString.append(i).append(". ").append(arr.get(i - 1)).append("\n");
+        }
+        return finalString.toString();
     }
 }
