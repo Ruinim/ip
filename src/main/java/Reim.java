2335619cfd8a6208d46af77e0909f7253d4fa48a
Ruinim <89561179+Ruinim@users.noreply.github.com>
2025-08-16 17:19:22 +0800

    Add echo

--- src/main/java/Reim.java
+++ src/main/java/Reim.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 public class Reim {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + "Hello! I'm Reim\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
+                + "____________________________________________________________\n";
+        String end = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        boolean stop = false;
+        while (!stop) {
+            Scanner read = new Scanner(System.in);
+            String command = read.nextLine();
+            String output = message(command);
+            if (command.equals("bye")) {
+                output = end;
+                stop = true;
+            }
+            System.out.println(output);
+        }
+
+
+
+
+    }
+
+    public static String message(String msg) {
+        return "____________________________________________________________\n"
+                + msg + "\n"
+                + "____________________________________________________________\n";
     }
 }
