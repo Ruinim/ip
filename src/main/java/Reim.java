a4879c95623d290dd169ad31b316cb0ea061c9bf
Ruinim <89561179+Ruinim@users.noreply.github.com>
2025-08-16 16:25:26 +0800

    Add greeting

---
+++ src/main/java/Reim.java
@@ -0,0 +1,11 @@
+public class Reim {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Reim\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
